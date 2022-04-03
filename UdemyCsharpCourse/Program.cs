8b5ebe4c70d682cb627eea8d3fbd367deaa0ab47
Alexey Bogoslovsky <lekhabog@outlook.com>
2022-04-03 23:19:04 +0300

    just types

--- UdemyCsharpCourse/Program.cs
+++ UdemyCsharpCourse/Program.cs
@@ -6,7 +6,22 @@ namespace CSharpcourse
     {
         static void Main(string [] arg)
         {
-            Console.WriteLine("Hello world!");
+            int x = -1;
+            int y;
+            y = 2;
+            float f = 2.3f;
+            double d = 2.3;
+            int i = new int();
+            var a = 1;
+            var b = 2.3;
+            decimal money = 3.0m;
+            char character = 'A';
+            string name = "John";
+            bool canDrive = true;
+            bool canDraw = false;
+            Console.WriteLine(a);
+            Console.WriteLine(name);
+            
         }
 
     }
