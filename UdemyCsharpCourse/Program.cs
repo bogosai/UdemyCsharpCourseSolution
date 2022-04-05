6a89656ca2e4172d88051e293617f0528daf648d
Alexey Bogoslovsky <lekhabog@outlook.com>
2022-04-05 22:34:43 +0300

    Basics homework in progress

--- UdemyCsharpCourse/Program.cs
+++ UdemyCsharpCourse/Program.cs
@@ -1,30 +1,36 @@
 ﻿using System;
 
 namespace CSharpcourse
-{ 
+{
     class Program
     {
-        static void Main(string [] arg)
-        {
-            int x = -1;
-            int y;
-            y = 2;
-            float f = 2.3f;
-            double d = 2.3;
-            int i = new int();
-            var a = 1;
-            var b = 2.3;
-            decimal money = 3.0m;
-            char character = 'A';
-            string name = "John";
-            bool canDrive = true;
-            bool canDraw = false;
-            Console.WriteLine(a);
-            Console.WriteLine(name);
-            
+        static void Main(String[] args) {
+            //BasicsCodingExcersise();
+            //BasicsHomeworkA();
+            BasicsHomeworkB();
         }
+        static void CodingExcersise() {
+            string name = "john snow";
+            int age = 29;
+            Console.WriteLine($"his name is {name} and he is {age} years old");
+            string name2 = "alexey";
+            int age2 = 45;
+            int difference = age2 - age;
+            Console.WriteLine($"...while his name is {name2}, he is {age2} year old, which is {difference} years older");
 
+        }
+        static void BasicsHomeworkA() {
+            Console.WriteLine("What is his name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+        static void BasicsHomeworkB()
+        {
+            Console.WriteLine("Give me any integer number");
+            int IntegerNumber = int.Parse(Console.ReadLine());
+            string IntegerNumberLength = IntegerNumber.ToString();
+            Console.WriteLine($"The length of your number is {IntegerNumberLength.Length}");
+
+        }
     }
 }
-
-
