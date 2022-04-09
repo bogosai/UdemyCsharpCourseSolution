3739b4069602f3c03d1e830ee2cccddaca1284e0
Alexey Bogoslovsky <lekhabog@outlook.com>
2022-04-09 17:58:20 +0300

    last section 3 homework to be done

--- UdemyCsharpCourse/Program.cs
+++ UdemyCsharpCourse/Program.cs
@@ -9,7 +9,12 @@ namespace CSharpcourse
             //BasicsHomeworkA();
             //BasicsHomeworkB();
             //BasicsHomeworkGeronFormula();
-            BasicsHomeworkProfile();
+            //BasicsHomeworkProfile();
+            //FlowHomeworkFindMaximum();            
+            //FibonachiHomework();
+            //averagehomework();
+            //factorialhomework();
+            authenticationhomework()
 
         }
         static void CodingExcersise() {
@@ -72,5 +77,105 @@ namespace CSharpcourse
             Console.WriteLine($"Your profile\nFull Name: {Lname + ',' + Fname}\nAge: {Age}\nWeight: {Weight}\nHeight: {Height}\nBody Mass Index: {imt}");
 
         }
+        static void FlowHomeworkFindMaximum()
+        {
+            Console.WriteLine("Value 1");
+            int Value1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Value 2");
+            int Value2 = int.Parse(Console.ReadLine());
+
+            bool BoolValue1 = Value1 > Value2;
+            bool BoolValue2 = Value1 < Value2; 
+            bool BoolEqual = Value1 == Value2;
+
+            if (BoolValue1) 
+            {
+                Console.WriteLine("Value1");
+            }
+            else if (BoolValue2)
+            {
+                Console.WriteLine("Value2");
+            }
+            else if (BoolEqual)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("fuck off");
+            }
+        }
+        static void FibonachiHomework()
+        {
+            Console.WriteLine("how many?");
+            int FibonachiNumbers = int.Parse(Console.ReadLine());            
+            int previous = 0;
+            int current = 1;
+            int newest = 0;
+            for (int i = 0; i < FibonachiNumbers; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write($"{current} ");
+                }
+                newest = current + previous;
+                previous = current;
+                current = newest;
+                Console.Write($"{newest} ");
+            }
+        }
+        static void averagehomework()
+        {
+            int intno = 0;
+            bool enough = intno == 0;
+            int count = 0;
+            double total = 0;
+            while (true)
+            {
+                
+                Console.WriteLine("give me some integer");
+                intno = int.Parse(Console.ReadLine());
+                if (intno == 0)
+                {
+                    break;
+                }                    
+                total = (double) total + (double) intno;                
+                count += 1;
+            }
+            Console.WriteLine($"Average is: {total = (double) total / (double) count}");
+
+        }
+        static void factorialhomework()
+        {
+            int intno = 0;
+            int fact = 1;
+            string check;
+            
+            while (true)
+            {
+                fact = 1;
+                Console.WriteLine("give me some integer");
+                check = Console.ReadLine();
+                if (check == "bye")
+                {
+                    break;
+                }
+                intno = int.Parse(check);
+                
+                for (int i = intno; i > 0; i--)
+                {
+                    fact = fact * i;
+                }
+                Console.WriteLine($"Factorial is: {fact}");
+            }
+            
+        }
+        static void authenticationhomework()
+        {
+
+        }
+
+
     }
+    
 }
